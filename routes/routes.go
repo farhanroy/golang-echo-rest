4ff1143a53cd14433e2d2a179e2ae591287a313a
farhanroy <farhanroy120@gmail.com>
2021-04-17 08:42:56 +0700

    :truck: adding api routes

---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"net/http"
+
+	"golang-echo-mysql/middleware"
+
+	"golang-echo-mysql/controllers"
+	"github.com/labstack/echo"
+)
+
+func Init() *echo.Echo {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, this is echo!")
+	})
+
+	e.GET("/pegawai", controllers.FetchAllPegawai, middleware.IsAuthenticated)
+	e.POST("/pegawai", controllers.StorePegawai, middleware.IsAuthenticated)
+	e.PUT("/pegawai", controllers.UpdatePegawai, middleware.IsAuthenticated)
+	e.DELETE("/pegawai", controllers.DeletePegawai, middleware.IsAuthenticated)
+
+	e.GET("/generate-hash/:password", controllers.GenerateHashPassword)
+	e.POST("/login", controllers.CheckLogin)
+
+	e.GET("/test-struct-validation", controllers.TestStructValidation)
+	e.GET("/test-variable-validation", controllers.TestVariableValidation)
+
+	return e
+}

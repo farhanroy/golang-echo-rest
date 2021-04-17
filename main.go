8f557027519c192da9482384dda39eac4e34ee21
farhanroy <farhanroy120@gmail.com>
2021-04-17 08:45:06 +0700

    :zap: main

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"net/http"
-	
-	"github.com/labstack/echo/v4"
+	"golang-echo-mysql/db"
+	"golang-echo-mysql/routes"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	db.Init()
+
+	e := routes.Init()
+
+	e.Logger.Fatal(e.Start(":1234"))
 }
